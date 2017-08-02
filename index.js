0e2a28992f655b5e10b418bf3b24ce54e4cac1fa
David Wang <dwang733@outlook.com>
2017-08-02 12:25:30 -0400

    Added search box for events w/ autocomplete

--- index.js
+++ index.js
@@ -17,6 +17,40 @@ function readExcelFile(url) {
     });
 }
 
+$.widget("custom.catcomplete", $.ui.autocomplete, {
+    _create: function() {
+        this._super();
+        this.widget().menu("option", "items", "> :not(.ui-autocomplete-category)");
+    },
+    _renderMenu: function(ul, items) {
+        const that = this;
+        let currentCategory = "";
+        $.each(items, function(index, item) {
+            let li;
+            if (item.category !== currentCategory) {
+                ul.append("<li class='ui-autocomplete-category'>" + item.category + "</li>");
+                currentCategory = item.category;
+            }
+            li = that._renderItemData(ul, item);
+            if (item.category) {
+                li.attr("aria-label", item.category + " : " + item.label);
+            }
+        });
+    }
+});
+
+$(".widget button").button();
+
+$("#searchbutton").click(() => {
+    const input = $("#search").val();
+    console.log(input);
+    $(".tooltip").each((i, obj) => {
+        if (obj.id === input) {
+            $(`[id="${input}"]`).tooltipster("open");
+        }
+    })
+});
+
 async function main() {
     try {
         const excelFilePath = "map_data.xlsx";
@@ -29,7 +63,7 @@ async function main() {
         const bStr = arr.join("");
         const workbook = XLSX.read(bStr, {type: "binary"});
 
-        const sizeMultiplier = 0.6;
+        const sizeMultiplier = 0.8;
         const img = $("#burnoutmap");
         img.height(img.height() * sizeMultiplier); // Changing only height maintains aspect ratio
         img.removeAttr("hidden");
@@ -38,6 +72,7 @@ async function main() {
         // Shift event destinations to beginning so events can reference them
         workbook.SheetNames.splice(workbook.SheetNames.indexOf("Event Destinations"), 1);
         workbook.SheetNames.unshift("Event Destinations");
+        const searchData = [];
         for (let i = 0; i < workbook.SheetNames.length; i++) {
             const sheetName = workbook.SheetNames[i];
             const sheet = workbook.Sheets[sheetName];
@@ -46,6 +81,7 @@ async function main() {
                 const eventName = sheet[`A${row}`].v;
                 const xCoord = sheet[`B${row}`].v * sizeMultiplier;
                 const yCoord = sheet[`C${row}`].v * sizeMultiplier;
+                const eventType = sheet[`D${row}`].v;
                 const areaRadius = sheetName === "Event Destinations" ? 0 : 7 * sizeMultiplier;
 
                 const elem = $("<area>");
@@ -67,17 +103,28 @@ async function main() {
                     elem.tooltipster({
                         functionBefore: () => dest.tooltipster("open"),
                         functionAfter: () => dest.tooltipster("close"),
-                        distance: 0 // Need this to correctly activate on event nodes
+                        distance: 0,
+                        trigger: "click"
                     });
                 } else {
-                    elem.tooltipster({distance: 0});
+                    elem.tooltipster({
+                        distance: 0,
+                        trigger: "click"
+                    });
                 }
                 elemFinish.tooltipster({distance: 0});
                 imageMap.append(elem);
                 imageMap.append(elemFinish);
                 row++;
+
+                searchData.push({label: eventName, category: eventType})
             }
         }
+
+        $("#search").catcomplete({
+            delay: 0,
+            source: searchData
+        })
     } catch (e) {
         console.error(e);
     }
